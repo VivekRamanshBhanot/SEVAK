b85bc8379a8ab86c42c864f633ef4577296d2169
VivekRamanshBhanot <73169663+VivekRamanshBhanot@users.noreply.github.com>
2021-02-13 15:47:26 +0530

    Making HEADC_Check file more readable

--- HEADC_Check/HEADC_Check.ino
+++ HEADC_Check/HEADC_Check.ino
@@ -1,33 +1,41 @@
 #include<Servo.h>
 Servo M;
 int HEsense,HEpin=A0,HED=0;
-
 int x;
 void setup() {
-  pinMode(7,OUTPUT);
-  digitalWrite(7,LOW);
- pinMode(HEpin,INPUT);
- M.attach(A2);
-Serial.begin(9600);
-M.write(86);
+    pinMode(7,OUTPUT);
+    digitalWrite(7,LOW);
+    pinMode(HEpin,INPUT);
+    M.attach(A2);
+    Serial.begin(9600);
+    M.write(86);
 }
-int HEADC()
+int HEADC()   //Hall Effect Analog To Digit Converter
 {
-  HEsense=analogRead(HEpin);
-  Serial.print(HEsense);
- Serial.print(" ");
-  if(HEsense<=611&&HEsense>=576)
-  {HED=600;return HED;}
-  else if(HEsense>=647)
-   {HED=650;return HED;}
+    HEsense=analogRead(HEpin);
+    Serial.print(HEsense);
+    Serial.print(" ");
+    if(HEsense<=611 && HEsense>=576)
+    {
+        HED=600;
+        return HED;
+    }
+    else if(HEsense>=647)
+    {
+        HED=650;
+        return HED;
+    }
    else if(HEsense<=557)
-   {HED=550;return HED;}
+   {
+       HED=550;
+       return HED;
+   }
    else
-   return HED;
+       return HED;
 }
 
 void loop() {
-x=HEADC();
-Serial.println(x);
-delay(10);
+    x=HEADC();
+    Serial.println(x);
+    delay(10);
 }
